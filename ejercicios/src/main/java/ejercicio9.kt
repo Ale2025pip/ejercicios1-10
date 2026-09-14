671d97606a4f2d5dcb53b49f3e37979af60783ed
Alejo <jano143217@gmail.com>
2026-09-14 18:34:57 -0300

    segundo guardado

--- ejercicios/src/main/java/ejercicio9.kt
+++ ejercicios/src/main/java/ejercicio9.kt
@@ -1,8 +1,8 @@
 enum class Planeta(val distanciaAlSol: Double){
-    MARTE(228.0,),
+    MARTE(228.0),
     JUPITER(distanciaAlSol = 778.0),
-    SATURNO(distanciaAlSol = 1433.0,),
-    URANO(distanciaAlSol = 2870.0,)
+    SATURNO(distanciaAlSol = 1433.0),
+    URANO(distanciaAlSol = 2870.0)
 }
 
 fun main(){
